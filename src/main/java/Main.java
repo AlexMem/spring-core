6f9683c9af7670dab927900a602b45fa1ce7d3cb
Aleksandr Burachenko <Aleksandr_Burachenko@epam.com>
2018-08-26 15:51:41 +0300

    6. PropertyFileApplicationContext

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
+import applicationcontexts.PropertyFileApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import quoters.Quoter;
 
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        PropertyFileApplicationContext context = new PropertyFileApplicationContext("context.property");
+        context.getBean(Quoter.class).sayQuote();
     }
 }
--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -11,6 +11,7 @@ import javax.annotation.PostConstruct;
 @DeprecatedClass(newImpl = T1000.class)
 public class TerminatorQuoter implements Quoter {
 
+
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
 
@@ -24,6 +25,10 @@ public class TerminatorQuoter implements Quoter {
 
     public TerminatorQuoter() {
         System.out.println("Phase 1");
+    }
+
+    public void setRepeat(int repeat) {
+        this.repeat = repeat;
     }
 
     public void setMessage(String message) {
