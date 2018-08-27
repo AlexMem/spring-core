5d4f0a5dfdbc3df3c9f86d92185750a296f15304
Aleksandr Burachenko <Aleksandr_Burachenko@epam.com>
2018-08-27 17:10:41 +0300

    hardcode entry removed (Task2)

--- src/main/java/com/andreitop/xml/config/AppConfig.java
+++ src/main/java/com/andreitop/xml/config/AppConfig.java
@@ -2,11 +2,11 @@ package com.andreitop.xml.config;
 
 import com.andreitop.xml.mount.*;
 import com.andreitop.xml.unit.*;
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.annotation.PropertySource;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -15,7 +15,11 @@ import java.util.concurrent.ThreadLocalRandom;
 
 @Configuration
 @ComponentScan(basePackages = "com.andreitop.xml")
+@PropertySource("classpath:config/heroes.properties")
 public class AppConfig {
+
+    @Value("${character.created}")
+    private String CHARACTER_CREATION_DATE;
 
     @Bean
     public Mount frostWolf() {
@@ -42,14 +46,7 @@ public class AppConfig {
 
     @Bean
     public SimpleDateFormat dateFormatter() {
-        return new SimpleDateFormat("dd-mm-yyyy");
-    }
-
-    @Bean
-    public PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
-        PropertyPlaceholderConfigurer configurer = new PropertyPlaceholderConfigurer();
-        configurer.setLocation(new ClassPathResource("config/heroes.properties"));
-        return configurer;
+        return new SimpleDateFormat("dd/mm/yyyy");
     }
 
     @Bean
@@ -62,7 +59,7 @@ public class AppConfig {
 
     @Bean
     public Set<Mount> trollMountSet() {
-        Set<Mount> set = new HashSet<>();
+        Set<Mount> set = new LinkedHashSet<>();
         set.add(shadowTiger());
         set.add(frostWolf());
         return set;
@@ -72,7 +69,7 @@ public class AppConfig {
     public Unit zulJin() throws ParseException {
         Troll zulJin = new Troll();
         zulJin.setColorCode(ThreadLocalRandom.current().nextInt(1, 10));
-        zulJin.setCreationDate(dateFormatter().parse("01-01-2004"));
+        zulJin.setCreationDate(dateFormatter().parse(CHARACTER_CREATION_DATE));
         zulJin.setListOfMounts(Arrays.asList(Troll.DEFAULT_MOUNT, null, shadowTiger()));
         zulJin.setSetOfMounts(trollMountSet());
         zulJin.setMapOfMounts(trollMountMap());
